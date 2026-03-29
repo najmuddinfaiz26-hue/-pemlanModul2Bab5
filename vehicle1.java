c0c3b6627830245a1a22e6c216d1c4e057330f30
Udeen <najmuddinfaiz26@gmail.com>
2026-03-30 06:34:26 +0700

    Implement vehicle1 class with load management

--- vehicle1.java
+++ vehicle1.java
@@ -1,1 +1,26 @@
-
+package modulbab5;
+//Muhamamd Faiz Najmuddin
+//255150707111013
+public class vehicle1{
+private double load;
+private static final double maxLoad = 10000;
+// public Vehicle1 (double max){
+// this.maxLoad = max;
+// }
+public double getLoad() {
+return this.load;
+}
+public double getMaxLoad(){
+    return this.maxLoad;
+}
+public boolean addBox(double weight){
+double temp = 0.0D;
+temp = this.load + weight;
+if(temp <= maxLoad){
+this.load = this.load + weight;
+return true;
+} else {
+return false;
+}
+}
+}
